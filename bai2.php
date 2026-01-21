e3ce1eb90c3fec4c700386f390f7b49eca673c98
thanhbn75 <minhthanhbn2005@gmail.com>
2026-01-21 07:56:32 +0700

    2commit

--- bai2.php
+++ bai2.php
@@ -1,19 +1,15 @@
 <?php
-// Khai báo class User
 class User {
-    // Thuộc tính public
     public $name;
     public $email;
     public $password;
 
-    // Constructor
     public function __construct($name, $email, $password) {
         $this->name = $name;
         $this->email = $email;
         $this->password = $password;
     }
 
-    // Method login
     public function login($inputEmail, $inputPassword) {
         if ($inputEmail === $this->email && $inputPassword === $this->password) {
             return "Đăng nhập thành công";
@@ -23,14 +19,11 @@ class User {
     }
 }
 
-// ===== TEST =====
-$user = new User("Nguyễn Văn A", "a@gmail.com", "123456");
+$user = new User("Vũ Minh Thành", "thanh@gmail.com", "123456");
 
-// Truy cập trực tiếp thuộc tính public
 echo "Tên: " . $user->name . "<br>";
 echo "Email: " . $user->email . "<br>";
 
-// Test login
-echo "test case1: ".$user->login("a@gmail.com", "123456");
-echo "<br>test case2: ".$user->login("b@gmail.com", "123456");
+echo "test case1: ".$user->login("thanh@gmail.com", "123456");
+echo "<br>test case2: ".$user->login("a@gmail.com", "123456");
 ?>
